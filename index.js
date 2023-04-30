a9090635771ed207090e7f6e0e3861d19d2c18ac
Juan-Corredor <juancs1921@gmail.com>
2023-04-30 16:40:45 -0500

    Mejoras funciones y apariencia

--- index.js
+++ index.js
@@ -1,68 +1,67 @@
-//Elementos HTML
-let boton = document.getElementById('boton');
-let valorVehiculo = document.getElementById("valorVehiculo");    
-
 //Variables de resultados
-let resultGlobal = document.getElementById('texto_1');
-let resultadosPeajeTotalA = document.getElementById('peaje_ta');
-let resultadosPeajeTotalC = document.getElementById('peaje_tc');
-let resultadosPeajeTotalM = document.getElementById('peaje_tm');
-
-let resultadosPeajesA = document.getElementById('peaje_a');
-let resultadosPeajesC = document.getElementById('peaje_c');
-let resultadosPeajesM = document.getElementById('peaje_m');
-
+const elementosHtml = {
+    boton: document.getElementById('boton'),
+    inputVehiculo: document.getElementById("valorVehiculo"),
+    peajesGlobal : document.getElementById('texto_1'),
+    totalAutomovil: document.getElementById('peaje_ta'),
+    totalCamion: document.getElementById('peaje_tc'),
+    totalMula: document.getElementById('peaje_tm'),
+    totalGeneral: document.getElementById('peajes_tt'),
+    peajesAutomovil: document.getElementById('peaje_a'),
+    peajesCamion: document.getElementById('peaje_c'),
+    peajesMula: document.getElementById('peaje_m'),
+}
 //Arreglos de tipos vehiculos
-let peajesAutomovil = [];
-let peajesCamion = [];
-let peajesMula = [];
-let totalPeajes = []
-//Variables para calcular totales individuales
-let peajeTotalA = 0;
-let peajeTotalC = 0;
-let peajeTotalM = 0;
-
-
-function valorInput(tipo){
-    return valorVehiculo.value = tipo
+const listPeajes = {
+    peajesAutomovil: [],
+    peajesCamion: [],
+    peajesMula: [],
+    peajeTotal: []
 }
 
-boton.onclick =  function( ){     
-    switch (valorVehiculo.value) {
-        case 'automovil':
-        case 'automóvil':
-            peajesAutomovil.push(15.300)
-            totalPeajes.push(15.300)
-          break;
-        case 'camion':
-        case 'camión':
-            peajesCamion.push(20.200)
-            totalPeajes.push(20.200)
-            break;
-        case 'mula':
-            peajesMula.push(25.500)
-            totalPeajes.push(25.500)
-            break;
-        default:
-          alert(`Lo siento, no se encontro el tipo de vehiculo ${valorVehiculo.value}.`);                  
-            break;          
-    }     
-    let resultados = `Los peajes recaudados de los ${totalPeajes.length} vehiculos fueron ${totalPeajes}.`
-    resultGlobal.innerHTML = resultados
-    calculoPeajeTotal()
-    
-    resultadosPeajesA.innerHTML =  peajesPorVehiculo('automóvil', peajesAutomovil )
-    resultadosPeajesC.innerHTML =  peajesPorVehiculo('camión', peajesCamion )
-    resultadosPeajesM.innerHTML =  peajesPorVehiculo('mula', peajesMula )
-    valorVehiculo.value = "";
-    return false;
+//Variables para calcular totales individuales y para formatear valor
+let peajeTotal = 0;
+const formatterNum = new Intl.NumberFormat('de-DE')
+
+// function valorInput(tipo){
+//     return elementosHtml.inputVehiculo.value = tipo;
+// }
+
+elementosHtml.boton.onclick =  function( ){ 
+    elementosHtml.peajesGlobal.innerHTML = calculoValorPeaje();
+    //Total de Peajes por vehiculos
+    elementosHtml.totalAutomovil.innerHTML =  peajesTotal('automóvil', listPeajes.peajesAutomovil, peajeTotal );        
+    elementosHtml.totalCamion.innerHTML =  peajesTotal('camión', listPeajes.peajesCamion, peajeTotal );   
+    elementosHtml.totalMula.innerHTML =  peajesTotal('mula', listPeajes.peajesMula, peajeTotal );
+    elementosHtml.totalGeneral.innerHTML =  peajesTotal('todos', listPeajes.peajeTotal, peajeTotal);
+    //Lista de peajes recaudados por vehiculos
+    elementosHtml.peajesAutomovil.innerHTML =  peajesPorVehiculo('automóvil', listPeajes.peajesAutomovil);
+    elementosHtml.peajesCamion.innerHTML =  peajesPorVehiculo('camión', listPeajes.peajesCamion);
+    elementosHtml.peajesMula.innerHTML =  peajesPorVehiculo('mula', listPeajes.peajesMula);       
+    return false;        
 };
 
-function calculoPeajeTotal(){
-    resultadosPeajeTotalA.innerHTML =  peajesTotal('automóvil', peajesAutomovil, peajeTotalA );        
-    resultadosPeajeTotalC.innerHTML =  peajesTotal('camión', peajesCamion, peajeTotalC );   
-    resultadosPeajeTotalM.innerHTML =  peajesTotal('mula', peajesMula, peajeTotalM );
-}    
+function calculoValorPeaje(){
+    switch (valorVehiculo.value) {        
+        case 'automóvil':
+            listPeajes.peajesAutomovil.push(15300)
+            listPeajes.peajeTotal.push(15300)
+          break;        
+        case 'camión':
+            listPeajes.peajesCamion.push(20200)
+            listPeajes.peajeTotal.push(20200)
+            break;
+        case 'mula':
+            listPeajes.peajesMula.push(25500)
+            listPeajes.peajeTotal.push(25500)
+            break;
+        default:
+          alert("Lo siento, debe elegir primero el tipo de vehiculo");                  
+            break;          
+    }     
+    let resultados = `Los peajes recaudados de los ${listPeajes.peajeTotal.length} vehiculos fueron: [${listPeajes.peajeTotal.map(formatterNum.format)}].`    
+    return resultados;
+}
 
 function peajesTotal(nameTipoVehiculo, listVehiculo, totalTipoPeaje){
     for (var v = 0; v < listVehiculo.length; v++) {
@@ -71,11 +70,13 @@ function peajesTotal(nameTipoVehiculo, listVehiculo, totalTipoPeaje){
         totalTipoPeaje += listVehiculo[n];                       
     }
 
-    let resultados = `Peaje total por ${nameTipoVehiculo}: ${totalTipoPeaje}`;                
+    let resultados = ``;                
+    nameTipoVehiculo == "todos" ? 
+        resultados = `Para un total de: ${formatterNum.format(totalTipoPeaje)}` : resultados = `Peaje total por ${nameTipoVehiculo}: ${formatterNum.format(totalTipoPeaje)}`
     return resultados;
 }
 
 function peajesPorVehiculo(nameTipoVehiculo, vehiculo){
-    let resultados = `Para el tipo de vehiculo "${nameTipoVehiculo}" los valores recaudados fueron: ${vehiculo}`;    
+    let resultados = `Para el tipo de vehiculo "${nameTipoVehiculo}" los valores recaudados fueron: [${vehiculo.map(formatterNum.format)}]`;    
     return resultados;
 }
